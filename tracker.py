ed20d74d3f613a8e261406e6e698b1dc6d48ab2a
Enigma7484 <omarhaque7484@gmail.com>
2023-01-04 17:02:09 -0500

    V1
    First Working Version

--- tracker.py
+++ tracker.py
@@ -10,6 +10,7 @@ CARBS_GOAL = 100 #g
 today = []
 
 
+@dataclass
 class FoodItem:
     name: str
     calories: int
@@ -26,3 +27,29 @@ while not done:
     (2) Visualize progress
     (3) Quit
     """)
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        print("Adding a new food item!")
+        name = input("Name: ")
+        calories = int(input("Calories: "))
+        proteins = int(input("Proteins: "))
+        fats = int(input("Fats: "))
+        carbs = int(input("Carbs: "))
+        food = FoodItem(name, calories, proteins, fats, carbs)
+        today.append(food)
+        print("Successfully added!")
+
+    elif choice == "2":
+        calorie_sum = sum(food.calories for food in today)
+        protein_sum = sum(food.protein for food in today)
+        fats_sum = sum(food.fat for food in today)
+        carbs_sum = sum(food.carbs for food in today)
+
+        fig, axs = plt.subplots(2, 2)
+        axs[0, 0].pie([protein_sum, fats_sum, carbs_sum], labels=["Proteins", "Fats", "Carbs"], autopct="%1.1f%%")
+        axs[0, 0].set_title("MacroNutrients Distribution")
+
+        fig.tight_layout()
+        plt.show()
